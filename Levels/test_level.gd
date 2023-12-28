39ec793e0f44fbc3b80341f853a38b99661c053b
Vanderbull <r.skeppstrom@gmail.com>
2023-12-28 08:06:52 +0100

    Fixed level transition
    Some minor flaws in the implementation was corrected.

--- Levels/test_level.gd
+++ Levels/test_level.gd
@@ -3,5 +3,6 @@ extends Area2D
 func _on_body_entered(body):
 	$"../CanvasLayer/Control/ending".show()
 	$"../CanvasLayer/Control/ending".text = str(1)
+	$"../CanvasLayer/Control/ending".show()
 	queue_free()
 	get_tree().change_scene_to_file("res://Levels/test_level2.tscn")
