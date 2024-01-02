1208fd78df3d97d4f00429d541f0ebabbfaeecaf
Vanderbull <r.skeppstrom@gmail.com>
2024-01-02 12:30:16 +0100

    Snow Particles
    Added snow particles to the first test level.

--- Levels/test_level.gd
+++ Levels/test_level.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+@export var scene_to_load : PackedScene
+
 const GameOverScreen = preload("res://UI/game_over_screen.tscn")
 @export var delay : float = 200.0
 
@@ -14,3 +16,7 @@ func _on_timer_timeout():
 	#var game_over = GameOverScreen.instantiate()
 	#add_child(game_over)
 	print("Timer stopped")
+
+
+func _on_check_button_toggled(_toggled_on):
+	get_tree().change_scene_to_packed(scene_to_load)
