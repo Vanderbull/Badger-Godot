3bfd331a629ff97a331ab4ea81ab9932a0991e66
Vanderbull <r.skeppstrom@gmail.com>
2024-01-05 13:31:20 +0100

    Music and Sound
    Added some basic background music and pickup sfx.

--- Levels/mushroom.gd
+++ Levels/mushroom.gd
@@ -1,16 +1,23 @@
 extends Area2D
 
+@onready var audio_player = $AudioStreamPlayer
+
 func _on_body_entered(body):
 	if get_tree().get_nodes_in_group("LevelOne"):
 		var Mushroom = get_tree().get_nodes_in_group("Mushroom")
 		print(Mushroom.size())
+		audio_player.play()
+		await audio_player.finished
 		queue_free()
+	
 		if Mushroom.size() == 1:
 			Events.level_completed.emit()
 			get_tree().change_scene_to_file("res://Levels/level_two.tscn")			
 	elif get_tree().get_nodes_in_group("LevelTwo"):
 		var Mushroom = get_tree().get_nodes_in_group("Mushroom")
 		print("Level 2: " + str(Mushroom.size()))
+		audio_player.play()
+		await audio_player.finished
 		queue_free()
 		#	$"../CanvasLayer/Label".text = "Mushrooms: " + str(Mushroom.size()-1)
 		if Mushroom.size() == 1:
