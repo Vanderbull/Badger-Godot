66122a0bf4bd98d1ed95e200b87bee79ab9771e7
Vanderbull <r.skeppstrom@gmail.com>
2024-01-14 13:29:07 +0100

    Pause menu
    Added pause menu

--- game_world.gd
+++ game_world.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+class_name GameWorld 
+
+signal toggle_game_paused(is_paused: bool)
+
+var game_paused : bool  = false:
+	get:
+		return game_paused
+	set(value):
+		game_paused = value
+		get_tree().paused = game_paused
+		emit_signal("toggle_game_paused", game_paused)
+		
 @export var next_level: PackedScene
 
 @onready var level_completed = $CanvasLayer/LevelCompleted
@@ -11,8 +23,15 @@ func _ready():
 	get_tree().paused = false
 	RenderingServer.set_default_clear_color(Color.BLACK)
 	Events.level_completed.connect(show_level_completed)
-	pass # Replace with function body.
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
+
+func _input(event):
+	if(event.is_action_pressed("ui_cancel")):
+		game_paused = !game_paused
+
+
+# Ending of the game
 func show_level_completed():
 	level_completed.show()
 	get_tree().paused = true
