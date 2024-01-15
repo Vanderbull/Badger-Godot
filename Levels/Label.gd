70c38fbabffdd060f6a196b93b02926802e5dbb3
Vanderbull <r.skeppstrom@gmail.com>
2024-01-15 10:40:25 +0100

    removed warnings
    Removed all the warnings in godot

--- Levels/Label.gd
+++ Levels/Label.gd
@@ -7,7 +7,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
 	if(get_tree().get_nodes_in_group("LevelOne")):
 		var Mushroom = get_tree().get_nodes_in_group("LevelOne")
 		text = "Mushrooms(one): " + str(Mushroom.size()-1)
